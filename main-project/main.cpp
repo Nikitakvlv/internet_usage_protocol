da9b982c492810a3983b8d9c9376f8e3e7f72811
Nikita <nikita2005719@gmail.com>
2024-05-28 00:45:50 +0300

    Итоговый вариант

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,59 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <algorithm>
 #include "utils.h"
-
 int main() {
-    std::vector<InternetSession> sessions;
-    std::ifstream infile("data/input.txt");
-
-    if (!infile) {
-        std::cerr << "Error opening file." << std::endl;
-        return 1;
-    }
-
-    std::string line;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        InternetSession session;
-        if (!(iss >> session.start.hours >> session.start.minutes >> session.start.seconds
-            >> session.end.hours >> session.end.minutes >> session.end.seconds
-            >> session.bytesReceived >> session.bytesSent)) {
-            break;
-        }
-        std::getline(iss, session.program);
-        sessions.push_back(session);
-    }
-    infile.close();
-
-    // Ôèëüòðàöèÿ ïî ïðîãðàììå Skype
-    auto skypeSessions = filterByProgram(sessions, "Skype");
-
-    // Ôèëüòðàöèÿ ïî âðåìåíè
-    Time threshold(8, 0, 0);
-    auto after8Sessions = filterByStartTime(sessions, threshold);
-
-    // Ñîðòèðîâêà
-    auto sortedByUsageTime = sessions;
-    insertionSort(sortedByUsageTime, usageTimeDescending);
-
-    auto sortedByProgram = sessions;
-    quickSort(sortedByProgram, programAscending);
-
-    // Âûâîä ðåçóëüòàòîâ
-    std::cout << "Sessions using Skype:\n";
-    printSessions(skypeSessions);
-
-    std::cout << "\nSessions after 8:00:00:\n";
-    printSessions(after8Sessions);
-
-    std::cout << "\nSessions sorted by usage time descending:\n";
-    printSessions(sortedByUsageTime);
-
-    std::cout << "\nSessions sorted by program name ascending:\n";
-    printSessions(sortedByProgram);
-
+    std::string filename = "data.txt";
+    filterAndSortData(filename);
     return 0;
 }
--- main-project/utils.cpp
+++ main-project/utils.cpp
@@ -1,92 +1,43 @@
 #include "utils.h"
-#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <algorithm>
+#include <iostream>
 
-bool Time::operator>(const Time& other) const {
-    return this->toSeconds() > other.toSeconds();
+bool sortByUsage(const InternetUsage& a, const InternetUsage& b) {
+    return (a.end_time > b.end_time);
 }
 
-int Time::toSeconds() const {
-    return hours * 3600 + minutes * 60 + seconds;
-}
-
-int InternetSession::usageTime() const {
-    return end.toSeconds() - start.toSeconds();
-}
-
-int InternetSession::totalBytes() const {
-    return bytesReceived + bytesSent;
-}
-
-std::vector<InternetSession> filterByProgram(const std::vector<InternetSession>& sessions, const std::string& programName) {
-    std::vector<InternetSession> result;
-    std::copy_if(sessions.begin(), sessions.end(), std::back_inserter(result), [&](const InternetSession& session) {
-        return session.program.find(programName) != std::string::npos;
-        });
-    return result;
-}
-
-std::vector<InternetSession> filterByStartTime(const std::vector<InternetSession>& sessions, const Time& threshold) {
-    std::vector<InternetSession> result;
-    std::copy_if(sessions.begin(), sessions.end(), std::back_inserter(result), [&](const InternetSession& session) {
-        return session.start > threshold;
-        });
-    return result;
-}
-
-void insertionSort(std::vector<InternetSession>& sessions, bool(*comp)(const InternetSession&, const InternetSession&)) {
-    for (size_t i = 1; i < sessions.size(); ++i) {
-        InternetSession key = sessions[i];
-        int j = i - 1;
-        while (j >= 0 && comp(sessions[j], key)) {
-            sessions[j + 1] = sessions[j];
-            --j;
-        }
-        sessions[j + 1] = key;
+bool sortByProgram(const InternetUsage& a, const InternetUsage& b) {
+    if (a.program != b.program) {
+        return a.program < b.program;
+    }
+    else {
+        return (a.received_bytes + a.sent_bytes) > (b.received_bytes + b.sent_bytes);
     }
 }
 
-int partition(std::vector<InternetSession>& sessions, int low, int high, bool(*comp)(const InternetSession&, const InternetSession&)) {
-    InternetSession pivot = sessions[high];
-    int i = (low - 1);
-    for (int j = low; j < high; ++j) {
-        if (!comp(sessions[j], pivot)) {
-            ++i;
-            std::swap(sessions[i], sessions[j]);
+void filterAndSortData(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    std::vector<InternetUsage> data;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        InternetUsage entry;
+        iss >> entry.start_time >> entry.end_time >> entry.received_bytes >> entry.sent_bytes >> entry.program;
+        if (entry.program == "Skype" && entry.start_time > "08:00:00") {
+            data.push_back(entry);
         }
     }
-    std::swap(sessions[i + 1], sessions[high]);
-    return (i + 1);
-}
-
-void quickSortUtil(std::vector<InternetSession>& sessions, int low, int high, bool(*comp)(const InternetSession&, const InternetSession&)) {
-    if (low < high) {
-        int pi = partition(sessions, low, high, comp);
-        quickSortUtil(sessions, low, pi - 1, comp);
-        quickSortUtil(sessions, pi + 1, high, comp);
+    file.close();
+    std::sort(data.begin(), data.end(), sortByUsage);
+    std::cout << "Sorted by usage time:" << std::endl;
+    for (const auto& entry : data) {
+        std::cout << entry.start_time << " - " << entry.end_time << ", Program: " << entry.program << std::endl;
     }
-}
-
-void quickSort(std::vector<InternetSession>& sessions, bool(*comp)(const InternetSession&, const InternetSession&)) {
-    quickSortUtil(sessions, 0, sessions.size() - 1, comp);
-}
-
-bool usageTimeDescending(const InternetSession& a, const InternetSession& b) {
-    return a.usageTime() > b.usageTime();
-}
-
-bool programAscending(const InternetSession& a, const InternetSession& b) {
-    if (a.program == b.program) {
-        return a.totalBytes() > b.totalBytes();
-    }
-    return a.program < b.program;
-}
-
-void printSessions(const std::vector<InternetSession>& sessions) {
-    for (const auto& session : sessions) {
-        std::cout << session.start.hours << ":" << session.start.minutes << ":" << session.start.seconds << " "
-            << session.end.hours << ":" << session.end.minutes << ":" << session.end.seconds << " "
-            << session.bytesReceived << " " << session.bytesSent << " "
-            << session.program << std::endl;
+    std::sort(data.begin(), data.end(), sortByProgram);
+    std::cout << "Sorted by program and usage:" << std::endl;
+    for (const auto& entry : data) {
+        std::cout << "Program: " << entry.program << ", Bytes: " << (entry.received_bytes + entry.sent_bytes) << std::endl;
     }
 }
